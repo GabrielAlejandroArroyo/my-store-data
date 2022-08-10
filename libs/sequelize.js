8e230c07030571465603aabd01f2ebd92d5fd94b
gabriel.arroyo@gmail.com <gabriel.arroyo@gmail.com>
2022-08-09 22:06:19 -0300

    Volviendo al la BD postgres

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -8,18 +8,15 @@ const USER_MYSQL = encodeURIComponent(config.dbUsermysql);
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 // Para BD postgres
-//const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
 // Para BD Mysql
-//const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
-
-const URI = `mysql://${USER_MYSQL}:${PASSWORD}@${config.dbHost}:${config.dbPortMysql}/${config.dbName}`
+//const URI = `mysql://${USER_MYSQL}:${PASSWORD}@${config.dbHost}:${config.dbPortMysql}/${config.dbName}`
 
 const sequelize = new Sequelize(URI, {
   // para BD Postgres
-  //dialect: 'postgres',
+  dialect: 'postgres',
   // para BD mysql
   //dialect: 'mysql',
-  dialect: 'mysql',
   logging: true,
 });
 
