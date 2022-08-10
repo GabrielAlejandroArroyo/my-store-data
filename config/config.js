b9e7de0fedd74b6a89cf31ccb0718503fa3b8114
gabriel.arroyo@gmail.com <gabriel.arroyo@gmail.com>
2022-08-09 21:50:27 -0300

    Cambio de BD con sequelize

--- config/config.js
+++ config/config.js
@@ -4,10 +4,12 @@ const config = {
   env: process.env.NODE_ENV || 'dev',
   port: process.env.PORT || '3000',
   dbUser: process.env.DB_USER,
+  dbUsermysql: process.env.DB_USER_MYSQL,
   dbPassword: process.env.DB_PASSWORD,
   dbHost: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
   dbPort: process.env.DB_PORT,
+  dbPortMysql: process.env.DB_PORT_MYSQL
 }
 
 module.exports = { config };
--- libs/sequelize.js
+++ libs/sequelize.js
@@ -3,13 +3,23 @@ const { Sequelize } = require('sequelize');
 const { config } = require('../config/config');
 const setupModels = require('../db/models');
 
-
+//const USER = encodeURIComponent(config.dbUser);
+const USER_MYSQL = encodeURIComponent(config.dbUsermysql);
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+// Para BD postgres
+//const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+// Para BD Mysql
+//const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+
+const URI = `mysql://${USER_MYSQL}:${PASSWORD}@${config.dbHost}:${config.dbPortMysql}/${config.dbName}`
 
 const sequelize = new Sequelize(URI, {
-  dialect: 'postgres',
+  // para BD Postgres
+  //dialect: 'postgres',
+  // para BD mysql
+  //dialect: 'mysql',
+  dialect: 'mysql',
   logging: true,
 });
 
